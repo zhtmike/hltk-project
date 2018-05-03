6344708ae3d76d2a4d2fa70033ed3ec8e0c4821b
mike <zhtmike@gmail.com>
2018-05-03 17:53:42 +0800

    update scripts

---
+++ script/build_model.sh
@@ -0,0 +1,22 @@
+#!/usr/bash
+
+#SBATCH -J hlta
+#SBATCH -p general
+#SBATCH -N 1 -n 20
+#SBATCH -t 24:00:00
+
+# batch version
+echo "*************** hlta_model_train_batch **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time --struct-use-all ./words/train/sample.sparse.txt 50 model_train_batch
+
+# step version (10000 sampled data cases, minibatch size)
+echo "*************** hlta_model_train_step **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/train/sample.sparse.txt 50 model_train_step
+
+# batch version
+echo "*************** hlta_model_full_batch **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time --struct-use-all ./words/full/sample.sparse.txt 50 model_full_batch
+
+# step version (10000 sampled data cases, minibatch size)
+echo "*************** hlta_model_full_step **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/full/sample.sparse.txt 50 model_full_step
--- script/prepocessing.sh
+++ script/prepocessing.sh
@@ -2,8 +2,8 @@
 
 # hole-out test
 # run the prepocessing for train and test set separately
-#java -cp HLTA.jar:HLTA-deps.jar tm.text.Convert -d ./words/train/sample ./data/train/ 2000 1
-#java -cp HLTA.jar:HLTA-deps.jar tm.text.Convert -d ./words/test/sample ./data/test/ 2000 1
+java -cp HLTA.jar:HLTA-deps.jar tm.text.Convert -d ./words/train/sample ./data/train/ 2000 1
+java -cp HLTA.jar:HLTA-deps.jar tm.text.Convert -d ./words/test/sample ./data/test/ 2000 1
 
 # build the model
 #java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA ./words/train/sample.sparse.txt 50 hlta_model
