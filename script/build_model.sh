b72f7d770d3e36d400dacace0a6c36ee67c0382c
mike <zhtmike@gmail.com>
2018-05-03 18:00:58 +0800

    update batch update number

--- script/build_model.sh
+++ script/build_model.sh
@@ -11,7 +11,7 @@ java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time --struct-use-all ./
 
 # step version (10000 sampled data cases, minibatch size)
 echo "*************** hlta_model_train_step **************"
-java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/train/sample.sparse.txt 50 model_train_step
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/train/sample.sparse.txt 100 model_train_step
 
 # batch version
 echo "*************** hlta_model_full_batch **************"
@@ -19,4 +19,4 @@ java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time --struct-use-all ./
 
 # step version (10000 sampled data cases, minibatch size)
 echo "*************** hlta_model_full_step **************"
-java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/full/sample.sparse.txt 50 model_full_step
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.HLTA --show-log-time -s 10000 -g 1000 ./words/full/sample.sparse.txt 100 model_full_step
