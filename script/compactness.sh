3866f2a39018f420f054f9e9445d26ffe207871c
mike <zhtmike@gmail.com>
2018-05-04 10:13:16 +0800

    add compactness

---
+++ script/compactness.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -J hlta_com
+#SBATCH -p general
+#SBATCH -N 1 -n 20
+#SBATCH -t 4:00:00
+
+# batch version
+echo "*************** hlta_model_batch **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.TopicCompactness batch.nodes.json ./words/full/sample.sparse.txt GoogleNews-vectors-negative300.bin
+
+# step version (10000 sampled data cases, 1000 minibatch size)
+echo "*************** hlta_model_step **************"
+java -cp HLTA.jar:HLTA-deps.jar tm.hlta.TopicCompactness step.nodes.json ./words/full/sample.sparse.txt GoogleNews-vectors-negative300.bin
